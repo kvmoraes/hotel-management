7c238224acb1df62993999360995be4ad94d8912
kvmoraes <karinevitoria.marinho@gmail.com>
2023-05-19 16:26:03 -0300

    docs: add README file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hotel Management
+
+This code has the purpose of manage and control the number of costumers checking in every day.
--- hotel.py
+++ hotel.py
@@ -1,8 +1,23 @@
+import os
+
+clients_folder = './clients'
+
 class Client:
     def __init__(self, name, age, children, **kwargs):
         self.name = name
         self.age = age
         self.children = children
 
-        for arg in kwargs:
+        for arg in kwargs.items():
             print(arg)
+
+def extract_client_info():
+    for file in os.listdir(clients_folder):
+        entire_path = os.path.join(clients_folder, file)
+
+        with open(entire_path, 'r') as f:
+            for line in f.readlines():
+                print(line)
+
+# client = Client('barbara', 22, 1, local = 'piscina', camas= 3)
+# print(client.name)
